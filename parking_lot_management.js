b145eef6effc323a4023360e304bd5697e29d54f
Dilshad <dilshad_fw11_177@masai.school>
2022-04-13 22:04:11 +0530

    parking lot manager component completed

--- parking_lot_management.js
+++ parking_lot_management.js
@@ -16,3 +16,44 @@ class Spot{
         this.vehicle = undefined;
     }
 }
+
+class ParkingLotManager{
+    constructor(vehicleTypes){
+        this.spots = [];
+        this.vehicles = new Map();
+    }
+    addSpot(vehicleType, ids){
+        ids.forEach((id)=>{
+            this.spots.push(new Spot({vehicleType, id}));
+        })
+    }
+
+    hasVehicle(licenseId){
+       return this.vehicles.has(licenseId)
+    }
+
+    placeVehicle(vehicle){
+        if(this.hasVehicle(vehicle.licenseId)){
+            console.log(`There is already a vehicle parked with id ${vehicle.licenseId}`)
+            return;
+         }
+         const spot = this.spots.find((spot)=> (vehicle instanceof spot.vehicleType) && this.isSpotFree())
+         if(spot){
+             this.vehicles.set(vehicle.licenseId, spot);
+             console.log(`Vehicle is parked at ${spot.id}`)
+         }else{
+             console.log(`There is no free spot for vehicle ${vehicle.licenseId}`)
+
+         }
+    }
+
+    removeVehicle(vehicle){
+        const spot = this.vehicles.get(vehicle.licenseId);
+        if(spot){
+            this.release();
+            this.vehicles.delete(vehicle.licenseId);
+        }else{
+            console.log("There is no vehicle with this Id")
+        }
+    }
+}
